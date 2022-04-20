dd180cd68dc57c26c3b4f3ac9e4a3047b745083d
선민기 <suns1502@gmail.com>
2022-04-20 16:57:11 +0900

    Feat : :sparkles: [BE] API Request Response 모델 정의
    펌뱅킹 파라메터 생성

--- back/core/src/main/java/com/parasol/core/api_model/AccountRequest.java
+++ back/core/src/main/java/com/parasol/core/api_model/AccountRequest.java
@@ -1,5 +1,6 @@
 package com.parasol.core.api_model;
 
+import com.parasol.core.eenum.TransactionType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
@@ -12,7 +13,7 @@ import lombok.ToString;
 @ApiModel("AccountRequest")
 public class AccountRequest {
     @ApiModelProperty(name = "method", example = "110-437-525252")
-    private String accountNo;
+    private TransactionType type;
     @ApiModelProperty(name = "amount", example = "4500000000")
     private int amount;
     @ApiModelProperty(name = "account_from", example = "")
