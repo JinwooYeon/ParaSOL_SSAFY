40e0882de38dcfd151308f8c854e576e0770f860
선민기 <suns1502@gmail.com>
2022-04-20 16:56:25 +0900

    Feat : :sparkles: [BE] 코어뱅킹 서비스 구현

--- back/core/src/main/java/com/parasol/core/service/TransactionHistoryService.java
+++ back/core/src/main/java/com/parasol/core/service/TransactionHistoryService.java
@@ -1,7 +1,34 @@
 package com.parasol.core.service;
 
+import com.parasol.core.api_model.AccountRequest;
+import com.parasol.core.entity.Account;
+import com.parasol.core.entity.TransactionHistory;
+import com.parasol.core.repository.AccountRepository;
+import com.parasol.core.repository.TransactionHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TransactionHistoryService {
+
+    @Autowired
+    private TransactionHistoryRepository transactionHistoryRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public TransactionHistory createAccountHistory(AccountRequest request) {
+
+
+        return null;
+    }
+
+    public List<TransactionHistory> getAccountHistory(String accountNo){
+        return transactionHistoryRepository.findByAccount_AccountNo(accountNo)
+                .stream().sorted(Comparator.comparing(TransactionHistory::getTransactionDate).reversed()).collect(Collectors.toList());
+    }
 }
