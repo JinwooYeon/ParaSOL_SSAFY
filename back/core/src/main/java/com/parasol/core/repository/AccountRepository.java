b6c25dbaaa1bce4573ee2cfc10d9459aa1dc2971
선민기 <suns1502@gmail.com>
2022-04-20 16:55:12 +0900

    Feat : :sparkles: [BE] 코어뱅킹 삽입 수정 삭제 조회 구현

--- back/core/src/main/java/com/parasol/core/repository/AccountRepository.java
+++ back/core/src/main/java/com/parasol/core/repository/AccountRepository.java
@@ -5,6 +5,12 @@ import com.parasol.core.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByAccountNo(String accountNo);
+    Optional<Account> findById(Long id);
+    List<Account> findAll();
 }
--- back/core/src/main/java/com/parasol/core/repository/TransactionHistoryRepository.java
+++ back/core/src/main/java/com/parasol/core/repository/TransactionHistoryRepository.java
@@ -4,6 +4,9 @@ import com.parasol.core.entity.TransactionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionHistoryRepository extends JpaRepository<TransactionHistory, Long> {
+    List<TransactionHistory> findByAccount_AccountNo(String accountNo);
 }
