d253feadce0923c9bf9137d1be4590861c7b39f1
선민기 <suns1502@gmail.com>
2022-04-20 12:51:57 +0900

    Style : :art: [BE] 트랜잭션 클래스 이름 수정
    Transaction -> TransactionHistory

--- back/core/src/main/java/com/parasol/core/entity/Account.java
+++ back/core/src/main/java/com/parasol/core/entity/Account.java
@@ -28,5 +28,5 @@ public class Account {
 
     @JsonManagedReference
     @OneToMany(mappedBy = "account")
-    private List<Transaction> transactions;
+    private List<TransactionHistory> transactionHistories;
 }
--- back/core/src/main/java/com/parasol/core/service/AccountService.java
+++ back/core/src/main/java/com/parasol/core/service/AccountService.java
@@ -3,16 +3,13 @@ package com.parasol.core.service;
 import com.parasol.core.entity.Account;
 import com.parasol.core.repository.AccountRepository;
 import com.parasol.core.utils.AccountManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
-
-    private final AccountRepository accountRepository;
-
-    public AccountService(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
+    @Autowired
+    private AccountRepository accountRepository;
 
     public String Create(Account account) {
         account.setAccountNo(AccountManager.GenerateAccountNumber());
