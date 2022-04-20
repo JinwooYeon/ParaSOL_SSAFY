f8c0a2149513b8136aecd98b7d6b04e2c8a7c837
선민기 <suns1502@gmail.com>
2022-04-20 16:55:12 +0900

    Feat : :sparkles: [BE] 코어뱅킹 삽입 수정 삭제 조회 구현

--- back/core/src/main/java/com/parasol/core/repository/ClientRepository.java
+++ back/core/src/main/java/com/parasol/core/repository/ClientRepository.java
@@ -5,9 +5,9 @@ import com.parasol.core.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-    Optional<Client> findByClientSeq(Long clientSeq);
 }
