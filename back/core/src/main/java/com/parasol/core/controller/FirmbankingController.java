f34dd52dfea947def3b50c9ea42efad4c08e7e62
선민기 <suns1502@gmail.com>
2022-04-20 17:56:05 +0900

    Feat : :sparkles: [BE] 펌뱅킹 조회 서비스 개선
    이름으로 검색
    이름과 타입으로 검색
    입금 계좌번호로 검색
    출금 계좌번호로 검색

--- back/core/src/main/java/com/parasol/core/controller/FirmbankingController.java
+++ back/core/src/main/java/com/parasol/core/controller/FirmbankingController.java
@@ -36,12 +36,36 @@ public class FirmbankingController {
         return "";
     }
 
-    // 이름으로 검색만 지원
-    @GetMapping("/")
-    public ResponseEntity<Firmbanking> getFirmbanking(
-            @RequestParam(name = "name", required = true) String name
+    // 이름으로 검색
+    @GetMapping("/name/{name}")
+    public ResponseEntity<Firmbanking> getFirmbankingByName(
+            @PathVariable(name = "이름") @ApiParam(value = "이름", required = true) String name
     ) {
-        return ResponseEntity.status(200).body(firmbankingService.getFirmbanking(name).get());
+        return ResponseEntity.status(200).body(firmbankingService.getFirmbankingByName(name).get());
     }
 
+    // 이름과 타입으로 검색
+    @GetMapping("/name/{name}/{type}")
+    public ResponseEntity<Firmbanking> getFirmbankingByNameAndType(
+            @PathVariable(name = "이름") @ApiParam(value = "이름", required = true) String name,
+            @PathVariable(name = "타입") @ApiParam(value = "타입", required = true) String type
+    ) {
+        return ResponseEntity.status(200).body(firmbankingService.getFirmbankingByNameAndType(name, type).get());
+    }
+
+    // 출금계좌번호로 검색
+    @GetMapping("/name/{withdrawAccount}")
+    public ResponseEntity<Firmbanking> getFirmbankingByWithdrawAccountNo(
+            @PathVariable(name = "출금계좌") @ApiParam(value = "출금계좌", required = true) String withdrawAccount
+    ) {
+        return ResponseEntity.status(200).body(firmbankingService.getFirmbankingByWithdrawAccount(withdrawAccount).get());
+    }
+
+    // 입금계좌번호로 검색
+    @GetMapping("/name/{depositAccount}")
+    public ResponseEntity<Firmbanking> getFirmbankingByDepositAccountNo(
+            @PathVariable(name = "입금계좌") @ApiParam(value = "입금계좌", required = true) String depositAccount
+    ) {
+        return ResponseEntity.status(200).body(firmbankingService.getFirmbankingByDeposiAccount(depositAccount).get());
+    }
 }
