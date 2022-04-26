34ab2844a0ce35c348c43cd21d2d249d8203f6c6
선민기 <suns1502@gmail.com>
2022-04-26 16:32:35 +0900

    Feat : :sparkles: [BE] 전문통신 더미 제작
    ClientFixed, AccountFixed

--- back/main/src/main/java/com/parasol/Main/service/AccountFixed.java
+++ back/main/src/main/java/com/parasol/Main/service/AccountFixed.java
@@ -1,25 +1,51 @@
 package com.parasol.Main.service;
 
-import com.parasol.Main.api_model.AccountResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Service
 public class AccountFixed {
-    public AccountResponse save(AccountResponse saveInfo) {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient fixedText;
+
+    /*
+    public AccountResponse createOpenAccountRequest(AccountOpenRequest accountInfo) {
         // 코어 뱅킹 시스템 rest api 호출
         return null;
     }
 
-    public List<AccountResponse> findById(String id) {
+    public AccountResponse createCloseAccountRequest(AccountCloseRequest saveInfo) {
         // 코어 뱅킹 시스템 rest api 호출
         return null;
     }
 
-    public List<AccountResponse> findByIdClient(AccountResponse account) {
+    public AccountResponse createQueryAccountListRequest(AccountResponse saveInfo) {
         // 코어 뱅킹 시스템 rest api 호출
         return null;
     }
+
+    public AccountResponse createQueryAccountBalanceRequest(AccountBalanceQueryRequest saveInfo) {
+        // 코어 뱅킹 시스템 rest api 호출
+        return null;
+    }
+
+    public AccountResponse createQueryAccountHistoryRequest(AccountHistoryQueryRequest saveInfo) {
+        // 코어 뱅킹 시스템 rest api 호출
+        return null;
+    }
+
+    public AccountResponse createDepositRequest(DepositRequest saveInfo) {
+        // 코어 뱅킹 시스템 rest api 호출
+        return null;
+    }
+
+    public AccountResponse createWithdrawRequest(WithdrawRequest saveInfo) {
+        // 코어 뱅킹 시스템 rest api 호출
+        return null;
+    }
+    */
 }
--- back/main/src/main/java/com/parasol/Main/service/ClientFixed.java
+++ back/main/src/main/java/com/parasol/Main/service/ClientFixed.java
@@ -1,23 +1,56 @@
 package com.parasol.Main.service;
 
 import com.parasol.Main.api_model.ClientResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @Service
 public class ClientFixed {
-    public ClientResponse save(ClientResponse saveInfo) {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient fixedText;
+
+    /*
+    public ClientResponse createAddClientRequest(RegisterRequest clientInfo) {
         // 코어 뱅킹 시스템 rest api 호출
         // http://corebank.com/client   - POST
-        WebClient webClient = WebClient.create("http://corebank.com");
+
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = fixedText.method(HttpMethod.POST);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account/")
+                .queryParam("name", "sun")
+                .queryParam("residentNumber", "202204261234567")
+                .build());
+
+        // ClientResponse에 응답 데이터 로드
+        // Mono<String> response = bodySpec.retrieve().bodyToMono(String.class);
+
         return null;
     }
+    */
 
-    public List<ClientResponse> findById(String id) {
+    /*
+    public List<ClientResponse> createRemoveClientRequest(AccountCloseRequest clientInfo) {
         // 코어 뱅킹 시스템 rest api 호출
         // http://corebank.com/client?id=아이디   - Get
+
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = fixedText.method(HttpMethod.DELETE);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account/")
+                .queryParam("name", "sun")
+                .queryParam("residentNumber", "202204261234567")
+                .build());
+
+        // ClientResponse에 응답 데이터 로드
+        // Mono<String> response = bodySpec.retrieve().bodyToMono(String.class);
+
         return null;
     }
+    */
 };
