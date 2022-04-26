8f480911636ea05afcfdad4b4e1552adcbf8f8c5
김형준 <ninini6630@gmail.com>
2022-04-26 14:54:05 +0900

    Feat : :sparkles: [BE] Account Service 메소드 생성

--- back/main/src/main/java/com/parasol/Main/service/AccountService.java
+++ back/main/src/main/java/com/parasol/Main/service/AccountService.java
@@ -6,7 +6,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountService {
 
-    public AccountBalance getBalance(String accountNo) {
+    public void create() {
+        return null;
+    }
+
+    public AccountBalanceResponse getAllAccount() {
+        return null;
+    }
+
+    public AccountBalanceResponse getBalance() {
+        return null;
+    }
+
+    public AccountHistoryResponse getHistory() {
+        return null;
+    }
+
+    public TransactionRequestResponse deposit() {
+        return null;
+    }
+
+    public TransactionRequestResponse withdraw() {
         return null;
     }
 }
