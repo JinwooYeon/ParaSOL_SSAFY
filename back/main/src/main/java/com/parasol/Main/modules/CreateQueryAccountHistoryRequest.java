b624a7a5550c61f9e393734deeeb10c62797330c
오나연 <cjcm1525@naver.com>
2022-04-27 11:53:57 +0900

    Feat : ✨ [BE] 계좌조회/잔액조회/거래내역조회 전문 요청 생성

--- back/main/src/main/java/com/parasol/Main/modules/CreateQueryAccountHistoryRequest.java
+++ back/main/src/main/java/com/parasol/Main/modules/CreateQueryAccountHistoryRequest.java
@@ -1,7 +1,29 @@
 package com.parasol.Main.modules;
 
+import com.parasol.Main.api_request.AccountHistoryQueryRequest;
+import com.parasol.Main.api_response.AccountHistoriesQueryResultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class CreateQueryAccountHistoryRequest {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient webClient;
+
+    public AccountHistoriesQueryResultResponse createQueryAccountHistoryRequest(AccountHistoryQueryRequest request) {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account/history")
+                .queryParam("accountNo", request.getBankAccountNumber())
+                .build());
+
+        Mono<AccountHistoriesQueryResultResponse> response = bodySpec.retrieve().bodyToMono(AccountHistoriesQueryResultResponse.class);
+
+        return response.block();
+    }
 }
--- back/main/src/main/java/com/parasol/Main/modules/CreateQueryAccountListRequest.java
+++ back/main/src/main/java/com/parasol/Main/modules/CreateQueryAccountListRequest.java
@@ -1,7 +1,30 @@
 package com.parasol.Main.modules;
 
+import com.parasol.Main.api_request.AccountListQueryRequest;
+import com.parasol.Main.api_response.AccountListQueryResultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class CreateQueryAccountListRequest {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient webClient;
+
+    public AccountListQueryResultResponse createQueryAccountListResponse(AccountListQueryRequest request) {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account")
+                .queryParam("name", request.getName())
+                .queryParam("residentNumber", request.getResidentNumber())
+                .build());
+
+        Mono<AccountListQueryResultResponse> response = bodySpec.retrieve().bodyToMono(AccountListQueryResultResponse.class);
+
+        return response.block();
+    }
 }
