4ce4a93ead01bb5083006eee881236ca9e292a60
김형준 <ninini6630@gmail.com>
2022-04-27 15:00:10 +0900

    Feat : ✨ BaaS Spring Boot 서버 프로젝트 생성

---
+++ back/service/src/test/java/com/parasol/BaaS/BaaSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasol.BaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
