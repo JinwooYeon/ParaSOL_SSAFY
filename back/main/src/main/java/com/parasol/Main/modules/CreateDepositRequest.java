6c95bc734adc6b1a08e9ec56415463b60fc3e489
선민기 <suns1502@gmail.com>
2022-04-27 11:54:36 +0900

    Feat : :sparkles: [BE] 예금 전문통신 구현

--- back/main/src/main/java/com/parasol/Main/modules/CreateDepositRequest.java
+++ back/main/src/main/java/com/parasol/Main/modules/CreateDepositRequest.java
@@ -1,7 +1,47 @@
 package com.parasol.Main.modules;
 
+import com.parasol.Main.api_model.AccountInfo;
+import com.parasol.Main.api_request.DepositRequest;
+import com.parasol.Main.api_response.TransactionExecuteResultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.UriSpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import reactor.core.publisher.Mono;
 
 @Component
 public class CreateDepositRequest {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient fixedText;
+
+    public TransactionExecuteResultResponse run(DepositRequest saveInfo){
+        /* 파라메터 */
+        MultiValueMap map = new LinkedMultiValueMap();
+        map.add("amount", 4500000000L);
+        map.add("account_from", saveInfo.getAccountFrom());
+        map.add("account_to", saveInfo.getAccountTo());
+
+        /* Http 통신 */
+        UriSpec<RequestBodySpec> uriSpec = fixedText.method(HttpMethod.POST);
+        RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account/deposit/")
+                .build());
+        RequestHeadersSpec<?> headersSpec = bodySpec.body(
+                BodyInserters.fromValue(map));
+
+        Mono<Boolean> response = headersSpec.retrieve().bodyToMono(Boolean.class);
+
+        /* TransactionExecuteResultResponse 에 응답 결과 담기 */
+        // Todo: ...
+
+        return null;
+    }
 }
