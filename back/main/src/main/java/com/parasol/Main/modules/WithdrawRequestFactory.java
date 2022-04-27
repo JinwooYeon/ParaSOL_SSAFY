d4ee4832eb8a6a26e7e3563163c3ba02be47be99
선민기 <suns1502@gmail.com>
2022-04-27 14:31:22 +0900

    Feat : :sparkles: [BE] 출금 전문통신 구현

--- back/main/src/main/java/com/parasol/Main/modules/WithdrawRequestFactory.java
+++ back/main/src/main/java/com/parasol/Main/modules/WithdrawRequestFactory.java
@@ -4,8 +4,11 @@ import com.parasol.Main.api_request.WithdrawRequest;
 import com.parasol.Main.api_response.TransactionExecuteResultResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class WithdrawRequestFactory {
@@ -14,6 +17,19 @@ public class WithdrawRequestFactory {
     private WebClient fixedText;
 
     public TransactionExecuteResultResponse run(WithdrawRequest saveInfo){
+        /* Http 통신 */
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = fixedText.method(HttpMethod.POST);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path("/account/withdraw")
+                .build());
+        WebClient.RequestHeadersSpec<?> headersSpec = bodySpec.body(BodyInserters.fromValue(saveInfo));
+        Mono<Boolean> response = headersSpec.retrieve().bodyToMono(Boolean.class);
+
+        /* TransactionExecuteResultResponse 에 응답 결과 담기 */
+        // Todo: ...
+
+        System.out.println(response.block());
+
         return null;
     }
 }
