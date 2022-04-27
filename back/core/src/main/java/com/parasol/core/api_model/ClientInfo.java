1b50a4cad76f6ad21a56b1af9fddb1c7e17f6871
선민기 <suns1502@gmail.com>
2022-04-28 01:52:33 +0900

    Fix : :bug: Core Banking 고객 이름, 고객 주민등록 번호 NotNull 설정

--- back/core/src/main/java/com/parasol/core/api_model/ClientInfo.java
+++ back/core/src/main/java/com/parasol/core/api_model/ClientInfo.java
@@ -6,13 +6,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @ToString
 @ApiModel("ClientInfo")
 public class ClientInfo {
     @ApiModelProperty(name="name", example="김싸피")
+    @NotBlank
     private String name;
     @ApiModelProperty(name="resident_number", example = "000000-0000000")
+    @NotBlank
     private String residentNumber;
 }
