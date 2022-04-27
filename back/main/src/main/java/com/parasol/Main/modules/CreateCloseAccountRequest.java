a943ed120e115e5d0241f4ab05b7beab3b019c66
김형준 <ninini6630@gmail.com>
2022-04-27 11:41:31 +0900

    Feat : ✨ [BE] 계좌 개설/폐쇄 전문 요청 생성

--- back/main/src/main/java/com/parasol/Main/modules/CreateCloseAccountRequest.java
+++ back/main/src/main/java/com/parasol/Main/modules/CreateCloseAccountRequest.java
@@ -1,7 +1,21 @@
 package com.parasol.Main.modules;
 
+import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class CreateCloseAccountRequest {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    WebClient webClient;
+
+    void createCloseAccountRequest() throws NotImplementedException {
+        // TODO: 코어 미구현으로 패스
+        throw new NotImplementedException("코어 미구현");
+    }
 }
--- back/main/src/main/java/com/parasol/Main/modules/CreateOpenAccountRequest.java
+++ back/main/src/main/java/com/parasol/Main/modules/CreateOpenAccountRequest.java
@@ -1,7 +1,29 @@
 package com.parasol.Main.modules;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class CreateOpenAccountRequest {
+    @Autowired
+    @Qualifier(value = "fixedText")
+    WebClient webClient;
+
+    void createOpenAccountRequest() {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = webClient.method(HttpMethod.POST);
+        WebClient.RequestHeadersSpec<?> bodySpec = uriSpec.uri("/account")
+                .body(BodyInserters
+                        .fromFormData("name", "sun")
+                        .with("residentNumber", "2022042-1234567")
+                        .with("accountPassword", "1234")
+                );
+        Mono<String> response = bodySpec.retrieve().bodyToMono(String.class);
+
+        System.out.println(response.block());
+    }
 }
