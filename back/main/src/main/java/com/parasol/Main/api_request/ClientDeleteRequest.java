f2d2a676493087ab6d33dce217842fffbc023bbe
김나경 <skruddl50@gmail.com>
2022-04-28 18:08:55 +0900

    Feat : :sparkles: [BE] Main API Server - api_request모델 Validation 추가

--- back/main/src/main/java/com/parasol/Main/api_request/ClientDeleteRequest.java
+++ back/main/src/main/java/com/parasol/Main/api_request/ClientDeleteRequest.java
@@ -1,6 +1,7 @@
 package com.parasol.Main.api_request;
 
 import com.parasol.Main.api_model.ClientInfo;
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +9,6 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@ApiModel("ClientDeleteRequest")
 public class ClientDeleteRequest extends ClientInfo {
 }
