9e0bc8cf7189b8630996baf4d4a2132d4f568f66
김형준 <ninini6630@gmail.com>
2022-04-28 14:34:06 +0900

    Feat : ✨ [BE] 잔액 조회 요청 Factory 클래스 생성

---
+++ back/service/src/main/java/com/parasol/BaaS/modules/QueryAccountBalanceRequestFactory.java
@@ -0,0 +1,46 @@
+package com.parasol.BaaS.modules;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.parasol.BaaS.api_request.DepositRequest;
+import com.parasol.BaaS.api_request.QueryAccountBalanceRequest;
+import com.parasol.BaaS.api_response.AccountBalanceQueryResultResponse;
+import com.parasol.BaaS.api_response.TransactionExecuteResultResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class QueryAccountBalanceRequestFactory {
+
+    @Autowired
+    @Qualifier(value = "fixedText")
+    private WebClient fixedText;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${sbj-api-server.balance}")
+    private String endPoint;
+
+    public AccountBalanceQueryResultResponse create(QueryAccountBalanceRequest request) throws JsonProcessingException {
+        WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = fixedText.method(HttpMethod.GET);
+        WebClient.RequestBodySpec bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
+                .path(endPoint)
+                .queryParam("bankName", request.getBankName())
+                .queryParam("bankAccountNumber", request.getBankAccountNumber())
+                .build()
+        );
+
+        Mono<String> response = bodySpec.retrieve().bodyToMono(String.class);
+
+        AccountBalanceQueryResultResponse formattedResponse = objectMapper.readValue(response.block(), AccountBalanceQueryResultResponse.class);
+
+        return formattedResponse;
+    }
+}
