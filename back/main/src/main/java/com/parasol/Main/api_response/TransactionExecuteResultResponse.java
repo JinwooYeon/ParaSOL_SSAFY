582cff4f26a05c9b62fd2c343fceba1577557152
김나경 <skruddl50@gmail.com>
2022-04-28 18:11:05 +0900

    Feat : :sparkles: [BE] Main API Server - api_response모델 Validation 추가

--- back/main/src/main/java/com/parasol/Main/api_response/TransactionExecuteResultResponse.java
+++ back/main/src/main/java/com/parasol/Main/api_response/TransactionExecuteResultResponse.java
@@ -1,6 +1,7 @@
 package com.parasol.Main.api_response;
 
 import com.parasol.Main.api_model.Transaction;
+import io.swagger.annotations.ApiModel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +9,6 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@ApiModel("TransactionExecuteResultResponse")
 public class TransactionExecuteResultResponse extends Transaction {
 }
