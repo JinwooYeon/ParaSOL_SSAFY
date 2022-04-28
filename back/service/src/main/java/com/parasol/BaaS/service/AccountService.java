df6a88e5ed486df49032475a4c7cd7b898cc4ef8
김형준 <ninini6630@gmail.com>
2022-04-28 14:35:38 +0900

    Feat : ✨ [BE] 잔액 조회 요청 서비스 로직 생성

--- back/service/src/main/java/com/parasol/BaaS/service/AccountService.java
+++ back/service/src/main/java/com/parasol/BaaS/service/AccountService.java
@@ -1,7 +1,34 @@
 package com.parasol.BaaS.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.parasol.BaaS.api_request.QueryAccountBalanceRequest;
+import com.parasol.BaaS.api_response.AccountBalanceQueryResultResponse;
+import com.parasol.BaaS.modules.QueryAccountBalanceRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
+
+    @Autowired
+    QueryAccountBalanceRequestFactory queryAccountBalanceRequestFactory;
+
+    public AccountBalanceQueryResultResponse getBalance(QueryAccountBalanceRequest request) {
+        String bankName = request.getBankName();
+        String accountNo = request.getBankAccountNumber();
+
+        try {
+            if (bankName != "SBJ") throw new IllegalArgumentException("We can support SBJ Bank only.");
+
+            AccountBalanceQueryResultResponse response = queryAccountBalanceRequestFactory.create(request);
+            return response;
+        } catch (JsonProcessingException e) {
+            System.out.println(e.toString());
+            return null;
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return null;
+        }
+    }
+
 }
