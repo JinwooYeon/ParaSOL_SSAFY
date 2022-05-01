e9c8566d8808ebf333e04366c044928b7909e390
김나경 <skruddl50@gmail.com>
2022-05-01 17:34:44 +0900

    Feat : :sparkles: [BE] Spring Security 초기 설정

---
+++ back/main/src/main/java/com/parasol/Main/config/SpringSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.parasol.Main.config;
+
+import com.parasol.Main.service.ClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final ClientService clientService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
