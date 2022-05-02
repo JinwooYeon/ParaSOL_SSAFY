fc89f54a7b90d68de0e85db28f064a30a82557ba
조항준 <wngkdroqkf441@gmail.com>
2022-05-02 09:59:29 +0900

    Feat : :sparkles: api 기본주소 환경변수 설정
    최상단에 .env 파일 생성 후 REACT_APP_BASE_URL=http://k6S101.p.ssafy.io:8080/ 작성

--- front/web/src/components/Components.tsx
+++ front/web/src/components/Components.tsx
@@ -12,7 +12,7 @@ interface PropsType {
 }
 
 export const Components: React.FC<PropsType> = (props: PropsType) => {
-  const BASE_URL = "http://k6s101.p.ssafy.io:8080";
+  // const BASE_URL = "http://k6S101.p.ssafy.io:8080/";
   const JWTtoken = localStorage.getItem("jwt")
     ? localStorage.getItem("jwt")
     : "";
@@ -26,46 +26,23 @@ export const Components: React.FC<PropsType> = (props: PropsType) => {
   const handleShow = () => {
     setShow(!show);
   };
-
   const onSubmit = async (data: any) => {
-    console.log(data);
     switch (props.API.method) {
       case "GET":
         await axios({
           method: "get",
           url: props.API.uri,
-          baseURL: BASE_URL,
+          // baseURL: BASE_URL,
           headers: { Authroization: `Bearer ${JWTtoken}` },
-          // `proxy`는 프록시 서버의 호스트이름, 포트, 프로토콜을 정의합니다.
-          // 기존의 `http_proxy`와 `https_proxy` 환경 변수를 사용하여
-          // 프록시를 정의할 수도 있습니다.
-          // 프록시 구성에 환경 변수를 사용하는 경우, 'no_proxy' 환경 변수를
-          // 쉼표로 구분된 프록시가 되지 않는 도메인 목록으로 정의할 수도 있습니다.
-          // `false`를 사용하면 프록시를 사용하지 않고, 환경 변수를 무시합니다.
-          // `auth`는 프록시에 연결하는데 HTTP Basic auth를 사용해야 함을 나타내며,
-          // 자격 증명을 제공합니다. 그러면 `Proxy-Authorization` 헤더가 설정되고,
-          // `headers`를 사용하여 기존의 `Proxy-Authorization` 사용자 지정 해더를 덮어씁니다.
-          // 만약 프록시 서버가 HTTPS를 사용한다면, 프로토콜을 반드시 `https`로 설정해야 합니다.
-          // proxy: {
-          //   protocol: 'https',
-          //   host: '127.0.0.1',
-          //   port: 9000,
-          //   auth: {
-          //     username: 'mikeymike',
-          //     password: 'rapunz3l'
-          //   }
-          // },
-          params: data,
+          // params: data,
         })
           .then((response) => {
-            console.log(response);
             setResponseData({
               status: response.status.toString(),
               output: response.data.toString(),
             });
           })
           .catch((err) => {
-            console.log(err);
             setResponseData({
               status: err.response.status.toString(),
               output: err.toString(),
@@ -76,12 +53,11 @@ export const Components: React.FC<PropsType> = (props: PropsType) => {
         await axios({
           method: "post",
           url: props.API.uri,
-          baseURL: BASE_URL,
+          // baseURL: BASE_URL,
           headers: { Authroization: `Bearer ${JWTtoken}` },
           data,
         })
           .then((response) => {
-            console.log(response);
             setResponseData({
               status: response.status.toString(),
               output: response.data.toString(),
@@ -90,8 +66,32 @@ export const Components: React.FC<PropsType> = (props: PropsType) => {
               localStorage.setItem("jwt", response.data.token);
             }
           })
+          // .catch(function (error) {
+          //   console.log(error);
+          //   if (error.response) {
+          //     // 요청이 이루어졌으며 서버가 2xx의 범위를 벗어나는 상태 코드로 응답했습니다.
+          //     console.log("first");
+          //     console.log(error.response.data);
+          //     console.log(error.response.status);
+          //     console.log(error.response.headers);
+          //   } else if (error.request) {
+          //     // 요청이 이루어 졌으나 응답을 받지 못했습니다.
+          //     // `error.request`는 브라우저의 XMLHttpRequest 인스턴스 또는
+          //     // Node.js의 http.ClientRequest 인스턴스입니다.
+          //     console.log("second");
+          //     console.log(error.request);
+          //   } else {
+          //     // 오류를 발생시킨 요청을 설정하는 중에 문제가 발생했습니다.
+          //     console.log("third");
+          //     console.log("Error", error.message);
+          //   }
+          //   console.log(error.config);
+          //   setResponseData({
+          //     status: error.response.status.toString(),
+          //     output: error.response.data.toString(),
+          //   });
+          // });
           .catch((err) => {
-            console.log(err);
             setResponseData({
               status: err.response.status.toString(),
               output: err.toString(),
@@ -102,19 +102,17 @@ export const Components: React.FC<PropsType> = (props: PropsType) => {
         await axios({
           method: "patch",
           url: props.API.uri,
-          baseURL: BASE_URL,
+          // baseURL: BASE_URL,
           headers: { Authroization: `Bearer ${JWTtoken}` },
           data,
         })
           .then((response) => {
-            console.log(response);
             setResponseData({
               status: response.status.toString(),
               output: response.data.toString(),
             });
           })
           .catch((err) => {
-            console.log(err);
             setResponseData({
               status: err.response.status.toString(),
               output: err.toString(),
@@ -125,19 +123,17 @@ export const Components: React.FC<PropsType> = (props: PropsType) => {
         await axios({
           method: "delete",
           url: props.API.uri,
-          baseURL: BASE_URL,
+          // baseURL: BASE_URL,
           headers: { Authroization: `Bearer ${JWTtoken}` },
           data,
         })
           .then((response) => {
-            console.log(response);
             setResponseData({
               status: response.status.toString(),
               output: response.data.toString(),
             });
           })
           .catch((err) => {
-            console.log(err);
             setResponseData({
               status: err.response.status.toString(),
               output: err.toString(),
