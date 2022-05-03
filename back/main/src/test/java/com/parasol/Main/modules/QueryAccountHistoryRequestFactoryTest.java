2b1077e489a6b8e1312335e484ee84934853e519
선민기 <suns1502@gmail.com>
2022-05-03 15:50:50 +0900

    Test : ☔️ [BE] Main 거래내역 조회 테스트

--- back/main/src/test/java/com/parasol/Main/modules/QueryAccountHistoryRequestFactoryTest.java
+++ back/main/src/test/java/com/parasol/Main/modules/QueryAccountHistoryRequestFactoryTest.java
@@ -1,39 +1,35 @@
 package com.parasol.Main.modules;
 
+import com.parasol.Main.api_model.AccountHistory;
 import com.parasol.Main.api_request.AccountHistoryQueryRequest;
 import com.parasol.Main.api_response.AccountHistoriesQueryResultResponse;
+import com.parasol.Main.api_response.TransactionExecuteResultResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 class QueryAccountHistoryRequestFactoryTest {
-
     @Autowired
     private QueryAccountHistoryRequestFactory queryAccountHistoryRequestFactory;
 
     @Test
     void createQueryAccountHistoryRequest() throws InterruptedException {
         AccountHistoryQueryRequest saveInfo = new AccountHistoryQueryRequest();
+        saveInfo.setBankAccountNumber("935-837-145298");
+        saveInfo.setBankName("pipi");
 
-        saveInfo.setBankName("SBJ");
-        saveInfo.setBankAccountNumber("188-158-441077");
+        Mono<List<AccountHistory>> result = queryAccountHistoryRequestFactory.createQueryAccountHistoryRequest(saveInfo);
 
-        List<Mono<AccountHistoriesQueryResultResponse>> result = new ArrayList<>();
+        result.subscribe(res ->{
+            for(AccountHistory tmp : res) {
+                System.out.println(tmp);
+            }
+        });
 
-        for(int i = 0;i < 1; i++){
-            result.add(queryAccountHistoryRequestFactory.createQueryAccountHistoryRequest(saveInfo));
-            result.get(i).subscribe(res -> {
-                System.out.println("response -> " + res);
-            });
-        }
-
-        Thread.sleep(5000);     // 다른 일 하는 중
-
+        Thread.sleep(5000);
     }
-
 }
