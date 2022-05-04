f9885f573636d4b4c157abfa34cb9e19e01db3ae
오나연 <cjcm1525@naver.com>
2022-05-04 13:19:40 +0900

    Fix : :bug: [BE] BaaS 로그인 로직 수정

--- back/service/src/main/java/com/parasol/BaaS/service/UserService.java
+++ back/service/src/main/java/com/parasol/BaaS/service/UserService.java
@@ -1,10 +1,16 @@
 package com.parasol.BaaS.service;
 
+import com.parasol.BaaS.api_model.AuthToken;
+import com.parasol.BaaS.api_model.LoginInfo;
+import com.parasol.BaaS.api_model.RefreshToken;
 import com.parasol.BaaS.api_model.UserInfo;
 import com.parasol.BaaS.api_request.LoginRequest;
 import com.parasol.BaaS.api_request.UserRegisterRequest;
 import com.parasol.BaaS.api_request.UserUpdateRequest;
+import com.parasol.BaaS.auth.jwt.util.JwtTokenUtil;
+import com.parasol.BaaS.db.entity.Token;
 import com.parasol.BaaS.db.entity.User;
+import com.parasol.BaaS.db.repository.TokenRepository;
 import com.parasol.BaaS.db.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,41 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private TokenRepository tokenRepository;
+
+    public AuthToken login(LoginRequest request) {
+        String id = request.getId();
+        String password = request.getPassword();
+
+        User user = getUserByUserId(id);
+
+        if(user == null) {
+            return null;
+        }
+
+        if(password.equals(user.getUserPassword())) {
+            AuthToken authToken = JwtTokenUtil.getToken(id);
+            String refreshToken = authToken.getRefreshToken().getRefreshToken();
+
+            Optional<Token> checkToken = tokenRepository.findByUser_UserId(id);
+
+            if(checkToken.isPresent()) {
+                Token newToken = checkToken.get();
+                newToken.setRefreshToken(refreshToken);
+                tokenRepository.save(newToken);
+            } else {
+                Token token = Token.builder()
+                        .user(user)
+                        .refreshToken(refreshToken)
+                        .build();
+                tokenRepository.save(token);
+            }
+            return authToken;
+        }
+        return null;
+    }
 
     public User getUserByUserId(String id) {
         Optional<User> oUser = userRepository.findByUserId(id);
