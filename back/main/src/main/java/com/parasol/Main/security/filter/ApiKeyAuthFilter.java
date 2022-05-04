53fa83cf0c48772d9af7a7e9ec26735633586266
선민기 <suns1502@gmail.com>
2022-05-05 02:50:40 +0900

    Feat : :sparkles: [BE] Main 인증 필터 생성

--- back/main/src/main/java/com/parasol/Main/security/filter/ApiKeyAuthFilter.java
+++ back/main/src/main/java/com/parasol/Main/security/filter/ApiKeyAuthFilter.java
@@ -1,18 +1,16 @@
 package com.parasol.Main.security.filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
-import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
 public class ApiKeyAuthFilter extends AbstractPreAuthenticatedProcessingFilter {
 
     // Todo : 클라이언트 아이디로 바꾸기
-    private final String API_KEY_AUTH_HEADER_NAME = "ClientId";
+    @Value("${authentication.baas.header}")
+    private String API_KEY_AUTH_HEADER_NAME;
 
-    @Autowired
     public ApiKeyAuthFilter(ApiKeyAuthManager manager) {
         this.setAuthenticationManager(manager);
     }
--- back/main/src/main/java/com/parasol/Main/security/filter/ApiKeyAuthManager.java
+++ back/main/src/main/java/com/parasol/Main/security/filter/ApiKeyAuthManager.java
@@ -4,22 +4,27 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@Component
 public class ApiKeyAuthManager implements AuthenticationManager {
     private String TEST_KEY;
+    private WebClient query;
 
     public ApiKeyAuthManager() {
+
     }
 
-    public ApiKeyAuthManager(String TEST_KEY) {
+    public ApiKeyAuthManager(String TEST_KEY, WebClient query) {
         this.TEST_KEY = TEST_KEY;
+        this.query = query;
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String principal = (String) authentication.getPrincipal();
+        /* JWT 토큰 검증 */
+        /* 1. 서명 검증 */
+        /* 2. 권한 확인 */
         if (!TEST_KEY.equals(principal)) {
             throw new BadCredentialsException("The API key was not found or not the expected value.");
         }
