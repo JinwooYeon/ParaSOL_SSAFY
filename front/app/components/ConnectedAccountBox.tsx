0ecfe6b188293f70a06bcdcaa19735800c58a437
연진우 <2dwd10@gmail.com>
2022-05-05 17:21:12 +0900

    Feat : :sparkles:  연결된 계좌 컴포넌트 개발

---
+++ front/app/components/ConnectedAccountBox.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import {
+  ConnectedAccountContainer,
+  ConnectedAccountContentContainer,
+  ConnectedAccountDetailBank,
+  ConnectedAccountDetailContainer,
+  ConnectedAccountDetailImg,
+  ConnectedAccountDetailNum,
+  ConnectedAccountHeaderContainer,
+  ConnectedAccountHeaderSetting,
+  ConnectedAccountHeaderSettingText,
+  ConnectedAccountHeaderText,
+} from "../screens/styled";
+
+interface PropsType {
+  bankInfo?: any;
+}
+
+const ConnectedAccountBox: React.FC<PropsType> = ({ bankInfo }) => {
+  const { bankImg, bankName, bankNum } = bankInfo;
+  const [empty, setEmpty] = useState(true);
+
+  const onPress = () => {
+    empty ? console.log("add") : console.log("setting");
+  };
+
+  useEffect(() => {
+    if (bankImg + bankName + bankNum !== "") setEmpty(false);
+  }, []);
+
+  return (
+    <ConnectedAccountContainer>
+      <ConnectedAccountHeaderContainer empty={empty}>
+        <ConnectedAccountHeaderText>연결된 계좌</ConnectedAccountHeaderText>
+        <ConnectedAccountHeaderSetting>
+          <ConnectedAccountHeaderSettingText onPress={onPress}>
+            {empty ? "추가" : "관리"}
+          </ConnectedAccountHeaderSettingText>
+        </ConnectedAccountHeaderSetting>
+      </ConnectedAccountHeaderContainer>
+      {empty ? null : (
+        <ConnectedAccountContentContainer>
+          <ConnectedAccountDetailImg source={{ uri: bankImg }} />
+          <ConnectedAccountDetailContainer>
+            <ConnectedAccountDetailBank>{bankName}</ConnectedAccountDetailBank>
+            <ConnectedAccountDetailNum>{bankNum}</ConnectedAccountDetailNum>
+          </ConnectedAccountDetailContainer>
+        </ConnectedAccountContentContainer>
+      )}
+    </ConnectedAccountContainer>
+  );
+};
+
+export default ConnectedAccountBox;
