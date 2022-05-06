5fd99834741f06047059d4cdc39071ce4fd129ae
김나경 <skruddl50@gmail.com>
2022-05-06 13:53:52 +0900

    Fix : :bug:  [BE]  Core 주민번호 유효성 검사 수정

--- back/core/src/main/java/com/parasol/core/api_model/ClientInfo.java
+++ back/core/src/main/java/com/parasol/core/api_model/ClientInfo.java
@@ -18,7 +18,7 @@ public class ClientInfo {
     @ApiModelProperty(name="name", example="김싸피")
     @NotBlank
     private String name;
-    @Pattern(regexp="/^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$/")
+    @Pattern(regexp="^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$")
     @ApiModelProperty(name="resident_number", example = "000000-0000000")
     @NotBlank
     @Size(max=14, min=14)
