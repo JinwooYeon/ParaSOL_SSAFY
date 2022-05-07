c3c31d3ab3baadd0d6cefcd5518cc1390dcde9ec
선민기 <suns1502@gmail.com>
2022-05-07 15:40:47 +0900

    Fix : :bug: Main Banking 인증 필터 수정
    특정 경로만 허가 -> 특정 경로를 제외한 모든 경로 허용

--- back/main/src/main/java/com/parasol/Main/config/SpringSecurityConfig.java
+++ back/main/src/main/java/com/parasol/Main/config/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -86,6 +87,16 @@ public class SpringSecurityConfig {
     @Order(2)
     @Configuration
     class Filter3 extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .cors()
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().permitAll();
+        }
+
         @Bean
         CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration config = new CorsConfiguration();
