41774acb00064ecf2366822ee95ada948a97c99e
김형준 <ninini6630@gmail.com>
2022-05-08 12:24:28 +0900

    Feat : ✨ [BE] gRPC 서버 Spring 서비스 생성

---
+++ back/core/src/main/java/com/parasol/core/grpc/GrpcServerService.java
@@ -0,0 +1,30 @@
+package com.parasol.core.grpc;
+
+import com.parasol.core.lib.AccountBalanceQueryRequest;
+import com.parasol.core.lib.AccountBalanceQueryResponse;
+import com.parasol.core.lib.CoreAPIGrpc;
+import com.parasol.core.service.AccountService;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@GrpcService
+public class GrpcServerService extends CoreAPIGrpc.CoreAPIImplBase {
+
+    @Autowired
+    AccountService accountService;
+
+    @Override
+    public void getBalance(AccountBalanceQueryRequest request, StreamObserver<AccountBalanceQueryResponse> responseObserver) {
+        String accountNumber = request.toString();
+
+        Long balance = accountService.getBalance(accountNumber);
+
+        AccountBalanceQueryResponse response = AccountBalanceQueryResponse.newBuilder()
+                .setBalance(balance.toString())
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
