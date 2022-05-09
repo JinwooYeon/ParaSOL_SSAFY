c5c69b8969bdf0801f8a836ab7965090a1adaea7
조항준 <wngkdroqkf441@gmail.com>
2022-05-09 14:16:39 +0900

    Feat : ✨ [FE] 계좌 목록 조회 컴포넌트 구현

--- front/app/screens/Account/ConnectAccount.tsx
+++ front/app/screens/Account/ConnectAccount.tsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
 import BtnBox from "../../components/BtnBox";
 import {
   ContentContainer,
@@ -7,16 +10,48 @@ import {
   LayoutContainer,
 } from "../styled";
 
-const ConnectAccount = ({ navigation }: any) => (
-  <LayoutContainer>
-    <HeaderText>계좌 관리하기</HeaderText>
-    <ContentFooterContainer>
-      <ContentContainer></ContentContainer>
-      <FooterContainer>
-        <BtnBox color="white" text="뒤로" navigation={navigation} />
-      </FooterContainer>
-    </ContentFooterContainer>
-  </LayoutContainer>
-);
+const ConnectAccount = ({ navigation }: any) => {
+  const [myAccounts, setMyAccounts] = useState([]);
+  const url = "http://k6S101.p.ssafy.io:8080/account";
+
+  const getMyAccounts = async () => {
+    await axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        setMyAccounts(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMyAccounts();
+  }, []);
+
+  return (
+    <LayoutContainer>
+      <HeaderText>계좌 관리하기</HeaderText>
+      <ContentFooterContainer>
+        <ContentContainer>
+          {myAccounts &&
+            myAccounts.map((account: any) => {
+              return (
+                <>
+                  <Text>bankName: {account.bankName}</Text>
+                  <Text>id: {account.id}</Text>
+                  <Text>password: {account.password}</Text>
+                </>
+              );
+            })}
+        </ContentContainer>
+        <FooterContainer>
+          <BtnBox color="white" text="뒤로" navigation={navigation} />
+        </FooterContainer>
+      </ContentFooterContainer>
+    </LayoutContainer>
+  );
+};
 
 export default ConnectAccount;
