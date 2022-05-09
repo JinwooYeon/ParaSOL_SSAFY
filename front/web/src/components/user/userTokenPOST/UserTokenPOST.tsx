eb0ca6911cea457bf077e52f24c25aa0a071f2a7
연진우 <2dwd10@gmail.com>
2022-05-09 11:50:30 +0900

    Fix : :bug: 모델명 변경

--- front/web/src/components/user/userTokenPOST/UserTokenPOST.tsx
+++ front/web/src/components/user/userTokenPOST/UserTokenPOST.tsx
@@ -1,5 +1,5 @@
 import { Components } from "components/Components";
-import { ReissueTokenRequest } from "model/Model";
+import { RefreshToken } from "model/Model";
 
 export const UserTokenPOST = () => {
   ////////////// 입력해야하는 부분 ///////////
@@ -7,10 +7,10 @@ export const UserTokenPOST = () => {
     uri: "/user/token",
     method: "POST",
     detail: "새로운 인증 토큰 요청",
-    completed: false,
+    completed: true,
   };
   const requestBody = {
-    ReissueTokenRequest,
+    RefreshToken,
   };
   ///////////////////////////////////
 
