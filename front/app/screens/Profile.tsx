5d1c24b95334806f78e752389db0147d8f457501
조항준 <wngkdroqkf441@gmail.com>
2022-05-09 10:48:22 +0900

    Feat : ✨ [FE] 회원정보 조회 기능 구현

--- front/app/screens/Profile.tsx
+++ front/app/screens/Profile.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import BtnBox from "../components/BtnBox";
 import {
   ContentContainer,
@@ -6,20 +7,47 @@ import {
   HeaderText,
   LayoutContainer,
 } from "./styled";
+import { Text } from "react-native";
+import axios from "axios";
 
-const Profile = ({ navigation }: any) => (
-  <LayoutContainer>
-    <HeaderText>회원정보</HeaderText>
-    <ContentFooterContainer>
-      <ContentContainer></ContentContainer>
-      <FooterContainer>
-        <BtnBox color="blue" text="정보 수정" navigation={navigation} />
-        <BtnBox color="blue" text="수정 완료" navigation={navigation} />
-        <BtnBox color="blue" text="비밀번호 수정" navigation={navigation} />
-        <BtnBox color="white" text="뒤로" navigation={navigation} />
-      </FooterContainer>
-    </ContentFooterContainer>
-  </LayoutContainer>
-);
+const Profile = ({ navigation }: any) => {
+  const [myInfo, setMyInfo] = useState([]);
+  const url = "http://k65101.p.ssafy.io:8080/user";
+
+  const getMyInfo = async () => {
+    await axios
+      .get(url)
+      .then((res) => {
+        console.log(res);
+        setMyInfo(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getMyInfo();
+  }, []);
+
+  return (
+    <LayoutContainer>
+      <HeaderText>회원정보</HeaderText>
+      <ContentFooterContainer>
+        <ContentContainer>
+          {myInfo.map((info) => {
+            return <Text>{info}</Text>;
+          })}
+        </ContentContainer>
+        <FooterContainer>
+          <BtnBox color="blue" text="정보 수정" navigation={navigation} />
+          <BtnBox color="blue" text="수정 완료" navigation={navigation} />
+          <BtnBox color="blue" text="비밀번호 수정" navigation={navigation} />
+          <BtnBox color="white" text="뒤로" navigation={navigation} />
+        </FooterContainer>
+      </ContentFooterContainer>
+    </LayoutContainer>
+  );
+};
 
 export default Profile;
