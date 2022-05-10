be547373293901f82e039dfd86f5f11f30143d84
김형준 <ninini6630@gmail.com>
2022-05-10 20:54:48 +0900

    Refactor : 💡 Core 인터페이스 Proto 모듈 및 Core 서버 Application 통합 및 멀티 모듈화

---
+++ back/core/application/core-application/src/main/java/com/parasol/core/repository/ClientRepository.java
@@ -0,0 +1,15 @@
+package com.parasol.core.repository;
+
+import com.parasol.core.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface ClientRepository extends JpaRepository<Client, String> {
+    Optional<Client> findByNameAndResidentNumber(String name, String residentNumber);
+    Optional<Client> findByBankUser_Id(String id);
+}
