50e860299b4aafd5594566f65f6c61641fb3d15d
sunminky <suns1502@gmail.com>
2022-05-10 11:11:30 +0900

    Fix : :bug:  [BE]  Core  Client 생성 시 주민등록번호 입력 삭제
    고객정보 생성 시 주민등록번호를 입력할 필요가 없어짐

--- back/core/src/main/java/com/parasol/core/controller/ClientController.java
+++ back/core/src/main/java/com/parasol/core/controller/ClientController.java
@@ -1,5 +1,6 @@
 package com.parasol.core.controller;
 
+import com.parasol.core.api_model.ClientInfo;
 import com.parasol.core.entity.Client;
 import com.parasol.core.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +16,15 @@ public class ClientController {
 
     @PostMapping("client")
     public String CreateClient(
-            @RequestBody @Valid Client client
+            @RequestBody @Valid ClientInfo client
     ) {
-        String result = clientService.create(client.getName(), client.getResidentNumber());
-
-        return result;
+        return clientService.create(client.getName());
     }
 
     @GetMapping("client")
     public Client GetClient(
             @RequestParam("id") String id
     ) {
-        Client result = clientService.findById(id);
-
-        return result;
+        return clientService.findById(id);
     }
 }
