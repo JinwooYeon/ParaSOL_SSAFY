5eb9634afb04fa198213064bb2351c53ead45d89
조항준 <wngkdroqkf441@gmail.com>
2022-05-10 11:50:38 +0900

    Feat : ✨ 비밀번호 재설정 컴포넌트 구현

--- front/app/screens/User/ForgetPassword.tsx
+++ front/app/screens/User/ForgetPassword.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Text, Button } from "react-native";
 import styled from "styled-components/native";
+import BtnBox from "../../components/BtnBox";
+import IdController from "../../components/Controller/IdController";
+import NameController from "../../components/Controller/NameController";
+import PasswordController from "../../components/Controller/PasswordController";
 
 const ContentContainer = styled.View`
   flex: 1;
@@ -12,13 +17,33 @@ interface PropsType {
   navigation: any;
 }
 
-const ForgetPassword: React.FC<PropsType> = ({ navigation: { goBack } }) => {
+const ForgetPassword: React.FC<PropsType> = ({ navigation }) => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const url = "http://k6s101.p.ssafy.io:8080/client/password";
+
+  const getNewPassword = async () => {
+    const data = {
+      id: id,
+      name: name,
+    };
+    await axios
+      .post(url, data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <ContentContainer>
       <Text>ForgetPassword</Text>
-
-      <Button title="Submit"></Button>
-      <Button onPress={() => goBack()} title="뒤로" />
+      <IdController setId={setId} text="아이디" />
+      <NameController setName={setName} text="이름" />
+      <BtnBox color="blue" text="비밀번호 재발급" setter={getNewPassword} />
+      <BtnBox color="white" text="뒤로" navigation={navigation} />
     </ContentContainer>
   );
 };
