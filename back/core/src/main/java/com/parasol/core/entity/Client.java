df7159ecff679edfe4ff6096f76fce9de2ac14b4
sunminky <suns1502@gmail.com>
2022-05-10 11:46:10 +0900

    Refactor : :bulb:  [BE]  Core ClientInfo 수정

--- back/core/src/main/java/com/parasol/core/entity/Client.java
+++ back/core/src/main/java/com/parasol/core/entity/Client.java
@@ -1,5 +1,6 @@
 package com.parasol.core.entity;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -7,6 +8,8 @@ import org.hibernate.validator.constraints.UniqueElements;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Entity
 @Getter
@@ -22,8 +25,11 @@ public class Client {
 
     @NotBlank
     private String name;
-    @NotBlank
     @Column(unique=true)
+    @Pattern(regexp="^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$")
+    @ApiModelProperty(name="resident_number", example = "000000-0000000")
+    @NotBlank
+    @Size(max=14, min=14)
     private String residentNumber;
 
     @OneToOne
