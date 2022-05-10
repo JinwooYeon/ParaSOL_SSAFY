bf272a1c79f370bc5b07aad5be385f65e1f0ad8a
김형준 <ninini6630@gmail.com>
2022-05-10 12:19:54 +0900

    Feat : ✨ [BE] BankUser 로그인 관련 요청, 반환 모델 생성

---
+++ back/core/src/main/java/com/parasol/core/api_model/BankUserLoginResponse.java
@@ -0,0 +1,18 @@
+package com.parasol.core.api_model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@ApiModel("BankUserLoginResponse")
+public class BankUserLoginResponse {
+    private boolean isSuccess;
+    private String cusno;
+}
