51a78e36bc39c1a01ef8df393116c30e748a0005
sunminky <suns1502@gmail.com>
2022-05-10 22:11:44 +0900

    Refactor : :bulb:   [BE] Main 목록 조회

--- back/main/src/main/java/com/parasol/Main/api_request/AccountHistoryQueryRequest.java
+++ back/main/src/main/java/com/parasol/Main/api_request/AccountHistoryQueryRequest.java
@@ -1,14 +1,19 @@
 package com.parasol.Main.api_request;
 
-import com.parasol.Main.api_model.AccountInfo;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @ToString
 @ApiModel("AccountHistoryQueryRequest")
-public class AccountHistoryQueryRequest extends AccountInfo {
+public class AccountHistoryQueryRequest{
+    @ApiModelProperty(name = "account_no", example = "110-437-525252")
+    @NotBlank
+    private String accountNo;
 }
