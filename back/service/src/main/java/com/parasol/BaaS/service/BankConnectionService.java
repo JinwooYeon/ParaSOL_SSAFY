df6cd8c896e8dec88aa94f8ca072b2f1ebc5df60
김형준 <ninini6630@gmail.com>
2022-05-11 16:05:48 +0900

    Feat : ✨ [BE] 은행 연결 서비스 로직 구현

--- back/service/src/main/java/com/parasol/BaaS/service/BankConnectionService.java
+++ back/service/src/main/java/com/parasol/BaaS/service/BankConnectionService.java
@@ -1,29 +1,57 @@
 package com.parasol.BaaS.service;
 
 import com.parasol.BaaS.api_request.BankConnectionRequest;
+import com.parasol.BaaS.api_request.BankLoginRequest;
+import com.parasol.BaaS.api_request.LoginRequest;
+import com.parasol.BaaS.api_response.BankLoginResultResponse;
 import com.parasol.BaaS.db.entity.BankConnection;
+import com.parasol.BaaS.db.entity.User;
 import com.parasol.BaaS.db.repository.BankConnectionRepository;
+import com.parasol.BaaS.modules.BankLoginRequestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import javax.transaction.Transactional;
+import java.util.Objects;
 
 @Service
 public class BankConnectionService {
 
     @Autowired
+    private BankLoginRequestFactory bankLoginRequestFactory;
+
+    @Autowired
     private BankConnectionRepository bankConnectionRepository;
 
-    public void addBankConnection(BankConnectionRequest request) throws IllegalArgumentException {
+    @Transactional
+    public void addBankConnection(User user, BankConnectionRequest request) throws IllegalArgumentException, NullPointerException {
         String bankName = request.getBankName();
         String id = request.getId();
         String password = request.getPassword();
 
-        // TODO: 각 값에 대한 검증 필요
-        BankConnection bankConnection = new BankConnection();
-        bankConnection.setBankName(bankName);
-        bankConnection.setBankId(id);
-        bankConnection.setBankPassword(password);
+        if (
+                bankName.isBlank() ||
+                id.isBlank() ||
+                password.isBlank()
+        )
+            throw new IllegalArgumentException();
 
-        bankConnectionRepository.save(bankConnection);
+        BankLoginRequest in_request = BankLoginRequest.builder()
+                .id(request.getId())
+                .password(request.getPassword())
+                .build();
+
+        BankLoginResultResponse response = bankLoginRequestFactory.create(in_request);
+        if (response.getIsSuccess()) {
+            BankConnection bankConnection = new BankConnection();
+            bankConnection.setBankName(bankName);
+            bankConnection.setBankId(id);
+            bankConnection.setBankPassword(password);
+            bankConnection.setUser(user);
+
+            bankConnectionRepository.save(bankConnection);
+        }
     }
 
 }
