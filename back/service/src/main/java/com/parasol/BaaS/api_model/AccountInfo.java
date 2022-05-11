dabeb1c55a3cbc7a15f5590b77877ec9b1c713cc
김형준 <ninini6630@gmail.com>
2022-05-11 23:10:01 +0900

    Feat : ✨ [BE] 계좌 정보 모델에 계좌번호 만 포함하도록 변경

--- back/service/src/main/java/com/parasol/BaaS/api_model/AccountInfo.java
+++ back/service/src/main/java/com/parasol/BaaS/api_model/AccountInfo.java
@@ -1,11 +1,14 @@
 package com.parasol.BaaS.api_model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class AccountInfo {
-    private String bankName;
-    private String bankAccountNumber;
+    private String accountNumber;
 }
