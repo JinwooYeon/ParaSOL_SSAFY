b3b110ce93c5babb1ea89d8cacc252d6643ee91d
김형준 <ninini6630@gmail.com>
2022-05-12 00:38:54 +0900

    Feat : ✨ [BE] 계좌 거래내역 반환시 은행명과 계좌번호를 함께 반환하도록 반환 모델 수정 및 Builder 패턴 도입

--- back/service/src/main/java/com/parasol/BaaS/api_response/AccountHistoryQueryResultResponse.java
+++ back/service/src/main/java/com/parasol/BaaS/api_response/AccountHistoryQueryResultResponse.java
@@ -2,9 +2,15 @@ package com.parasol.BaaS.api_response;
 
 import com.parasol.BaaS.api_model.AccountHistories;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class AccountHistoryQueryResultResponse extends AccountHistories {
+    private String bankName;
+    private String bankAccountNumber;
 }
