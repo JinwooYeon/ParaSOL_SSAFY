173b0320a0eba26dd6ac23a8ff9347daaef7c704
김형준 <ninini6630@gmail.com>
2022-05-12 01:14:57 +0900

    Feat : ✨ [BE] 계좌 정보 모델에서 불필요한 은행명 컬럼 제거

--- back/main/src/main/java/com/parasol/Main/api_model/AccountInfo.java
+++ back/main/src/main/java/com/parasol/Main/api_model/AccountInfo.java
@@ -2,9 +2,8 @@ package com.parasol.Main.api_model;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.lang.Nullable;
 
 import javax.validation.constraints.NotBlank;
@@ -12,12 +11,12 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 @ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @ApiModel("AccountInfo")
 public class AccountInfo {
-    @ApiModelProperty(name = "bank_name", example = "피피쿠스은행")
-    @Nullable
-    private String bankName;
-    @ApiModelProperty(name = "bank_account_number", example = "110-437-525252")
+    @ApiModelProperty(name = "account_number", example = "110-437-525252")
     @NotBlank
-    private String bankAccountNumber;
+    private String accountNumber;
 }
