39d094325847ddb93ca2a1126ca1fe603ed79a6c
오나연 <cjcm1525@naver.com>
2022-05-11 11:10:31 +0900

    Fix : :bug: [BE] PasswordResetRequest 상속 관계 수정

--- back/service/src/main/java/com/parasol/BaaS/api_request/PasswordResetRequest.java
+++ back/service/src/main/java/com/parasol/BaaS/api_request/PasswordResetRequest.java
@@ -1,10 +1,10 @@
 package com.parasol.BaaS.api_request;
 
-import com.parasol.BaaS.api_model.Password;
+import com.parasol.BaaS.api_model.UserInfo;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class PasswordResetRequest extends Password {
+public class PasswordResetRequest extends UserInfo {
 }
