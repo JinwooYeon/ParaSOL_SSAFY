1faa0d79b1d8d4d6abe0be8253a6a7fdad7807ef
김형준 <ninini6630@gmail.com>
2022-05-11 23:59:40 +0900

    Feat : ✨ [BE] 계좌 잔액 모델에서 계좌 잔액 조회 반환 모델로 은행명, 계좌번호 컬럼 이동

--- back/service/src/main/java/com/parasol/BaaS/api_response/AccountBalanceQueryResultResponse.java
+++ back/service/src/main/java/com/parasol/BaaS/api_response/AccountBalanceQueryResultResponse.java
@@ -1,10 +1,15 @@
 package com.parasol.BaaS.api_response;
 
 import com.parasol.BaaS.api_model.AccountBalance;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AccountBalanceQueryResultResponse extends AccountBalance {
+    private String bankName;
+    private String bankAccountNumber;
 }
