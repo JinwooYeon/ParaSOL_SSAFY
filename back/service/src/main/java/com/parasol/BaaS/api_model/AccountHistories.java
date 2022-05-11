5808bf87b59eabd260e825a9d1a5e48ebd65662e
김형준 <ninini6630@gmail.com>
2022-05-12 00:37:53 +0900

    Feat : ✨ [BE] 계좌 거래내역 목록 모델에 Builder 패턴 도입

--- back/service/src/main/java/com/parasol/BaaS/api_model/AccountHistories.java
+++ back/service/src/main/java/com/parasol/BaaS/api_model/AccountHistories.java
@@ -1,12 +1,16 @@
 package com.parasol.BaaS.api_model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class AccountHistories {
-    private List<AccountHistory> accountHistories;
+    private List<AccountHistory> bankAccountHistories;
 }
