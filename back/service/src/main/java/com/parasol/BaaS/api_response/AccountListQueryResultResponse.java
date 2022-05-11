2806440771370224b7b0500e8e3c3f121923058a
김형준 <ninini6630@gmail.com>
2022-05-11 23:12:52 +0900

    Feat : ✨ [BE] 은행명을 상속받지 않고 요청 모델이 직접 소유하도록 변경

--- back/service/src/main/java/com/parasol/BaaS/api_response/AccountListQueryResultResponse.java
+++ back/service/src/main/java/com/parasol/BaaS/api_response/AccountListQueryResultResponse.java
@@ -2,9 +2,14 @@ package com.parasol.BaaS.api_response;
 
 import com.parasol.BaaS.api_model.AccountList;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class AccountListQueryResultResponse extends AccountList {
+    private String bankName;
 }
