199f202738151e362e90d102dd85155aa3788faa
김형준 <ninini6630@gmail.com>
2022-05-11 23:10:43 +0900

    Feat : ✨ [BE] 계좌 리스트 모델의 계좌 정보 리스트의 컬럼명을 변경

--- back/service/src/main/java/com/parasol/BaaS/api_model/AccountList.java
+++ back/service/src/main/java/com/parasol/BaaS/api_model/AccountList.java
@@ -1,12 +1,16 @@
 package com.parasol.BaaS.api_model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.util.List;
 
 @Getter
 @Setter
+@SuperBuilder
+@NoArgsConstructor
 public class AccountList {
-    private List<AccountInfo> accounts;
+    private List<AccountInfo> bankAccounts;
 }
