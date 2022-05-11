95e19c68e08223ba080007978a3ed959d703bac4
김형준 <ninini6630@gmail.com>
2022-05-11 23:16:11 +0900

    Feat : ✨ [BE] 은행명과 유저 번호를 통해 뱅킹 연결 정보를 얻어올 수 있도록 리포지토리 변경

--- back/service/src/main/java/com/parasol/BaaS/db/repository/BankConnectionRepository.java
+++ back/service/src/main/java/com/parasol/BaaS/db/repository/BankConnectionRepository.java
@@ -2,6 +2,11 @@ package com.parasol.BaaS.db.repository;
 
 import com.parasol.BaaS.db.entity.BankConnection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BankConnectionRepository extends JpaRepository<BankConnection, Long> {
+    Optional<BankConnection> findByUser_UserSeqAndBankName(Long userSeq, String bankName);
 }
