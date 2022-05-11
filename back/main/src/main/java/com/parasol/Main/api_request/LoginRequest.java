c34900de4aadfb2535cff1d4e0a391363178e7a8
김형준 <ninini6630@gmail.com>
2022-05-11 23:20:53 +0900

    Feat : ✨ [BE] 로그인 요청 모델에 Builder 패턴 적용

--- back/main/src/main/java/com/parasol/Main/api_request/LoginRequest.java
+++ back/main/src/main/java/com/parasol/Main/api_request/LoginRequest.java
@@ -2,13 +2,14 @@ package com.parasol.Main.api_request;
 
 import com.parasol.Main.api_model.LoginInfo;
 import io.swagger.annotations.ApiModel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @ToString
+@SuperBuilder
+@NoArgsConstructor
 @ApiModel("LoginRequest")
 public class LoginRequest extends LoginInfo {
 }
