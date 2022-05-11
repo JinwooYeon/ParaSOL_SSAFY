3dc1f796745a6d1c2853852186679e971911dc34
오나연 <cjcm1525@naver.com>
2022-05-11 11:12:41 +0900

    Feat : :sparkles: [BE] 임시 비밀번호 생성, 설정 로직 추가

--- back/service/src/main/java/com/parasol/BaaS/service/UserService.java
+++ back/service/src/main/java/com/parasol/BaaS/service/UserService.java
@@ -3,10 +3,8 @@ package com.parasol.BaaS.service;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.parasol.BaaS.api_model.AuthToken;
-import com.parasol.BaaS.api_request.LoginRequest;
-import com.parasol.BaaS.api_request.ReissueTokenRequest;
-import com.parasol.BaaS.api_request.UserRegisterRequest;
-import com.parasol.BaaS.api_request.UserUpdateRequest;
+import com.parasol.BaaS.api_model.Password;
+import com.parasol.BaaS.api_request.*;
 import com.parasol.BaaS.auth.jwt.JwtAuthenticationFilter;
 import com.parasol.BaaS.auth.jwt.UserDetail;
 import com.parasol.BaaS.auth.jwt.util.JwtTokenUtil;
@@ -23,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class UserService {
@@ -34,8 +33,6 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
-    private AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public AuthToken login(LoginRequest request) {
         String id = request.getId();
@@ -93,6 +90,35 @@ public class UserService {
             tokenRepository.save(token);
         }
         return authToken;
+    }
+
+    public Password resetPassword(PasswordResetRequest request) {
+        String id = request.getId();
+        String name = request.getName();
+
+        if(!StringUtils.hasText(id) || !StringUtils.hasText(name)) {
+            return null;
+        }
+
+        Optional<User> oUser = userRepository.findByUserId(id);
+
+        if(!oUser.isPresent()) {
+            return null;
+        }
+
+        User user = oUser.get();
+        if(name.equals(user.getUserName())) {
+            String newPassword = createPassword();
+
+            user.setUserPassword(passwordEncoder.encode(newPassword));
+
+            userRepository.save(user);
+
+            return Password.builder()
+                    .password(newPassword)
+                    .build();
+        }
+        return null;
     }
 
     // AuthToken 재발급
@@ -163,4 +189,31 @@ public class UserService {
         if(tokenDelete > 0 && delete > 0) return true;
         return false;
     }
+
+    // 임시 비밀번호 8자리
+    public String createPassword() {
+        StringBuffer password = new StringBuffer();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) { // 8자리
+            int index = rnd.nextInt(3); // 0~2 까지 랜덤
+
+            switch (index) {
+                case 0:
+                    password.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
+                    break;
+                case 1:
+                    password.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    //  A~Z
+                    break;
+                case 2:
+                    password.append((rnd.nextInt(10)));
+                    // 0~9
+                    break;
+            }
+        }
+        return password.toString();
+    }
+
 }
