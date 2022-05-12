5f144c251251030d1b3b2c8fa5a795ec14513a81
김형준 <ninini6630@gmail.com>
2022-05-12 19:58:35 +0900

    Refactor : :bulb: [BE] 회원 관련 컨트롤러 리팩토링

--- back/service/src/main/java/com/parasol/BaaS/controller/UserController.java
+++ back/service/src/main/java/com/parasol/BaaS/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.parasol.BaaS.controller;
 
-import com.parasol.BaaS.api_model.AuthToken;
-import com.parasol.BaaS.api_model.Password;
 import com.parasol.BaaS.api_model.UserInfo;
 import com.parasol.BaaS.api_request.*;
-import com.parasol.BaaS.api_response.AuthTokenResponse;
-import com.parasol.BaaS.api_response.UserInfoQueryResultResponse;
+import com.parasol.BaaS.api_response.*;
 import com.parasol.BaaS.auth.jwt.UserDetail;
 import com.parasol.BaaS.db.entity.User;
 import com.parasol.BaaS.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import reactor.core.publisher.Mono;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -21,164 +24,112 @@ import java.io.IOException;
 @RequestMapping("/user")
 public class UserController {
 
-    @Value("${sbj-api-server.base-url}") String baseUrl;
-
     @Autowired
     private UserService userService;
 
     @PostMapping("/login")
-    public AuthTokenResponse login(
+    public Mono<ResponseEntity<LoginResponse>> login (
             @RequestBody LoginRequest request
     ) {
-
-        AuthToken token = userService.login(request);
-
-        if(token == null) {
-            return null;
-        }
-
-        return AuthTokenResponse.builder()
-                .accessToken(token.getAccessToken().getAccessToken())
-                .refreshToken(token.getRefreshToken().getRefreshToken())
-                .build();
+        return userService.login(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @GetMapping("/login/google")
-    public void loginGoogle(HttpServletResponse response) throws IOException {
+    public void loginGoogle(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        String baseUrl = ServletUriComponentsBuilder
+                .fromRequestUri(request)
+                .replacePath(null)
+                .build()
+                .toUriString();
+
         String redirectUri = baseUrl + "/oauth2/authorization/google";
         response.sendRedirect(redirectUri);
     }
 
     @GetMapping("/login/oauth")
-    public AuthTokenResponse loginOauth(@RequestParam String id) {
-        if(id == null) {
-            return null;
-        }
-        AuthToken token = userService.loginOauth(id);
-
-        return AuthTokenResponse.builder()
-                .accessToken(token.getAccessToken().getAccessToken())
-                .refreshToken(token.getRefreshToken().getRefreshToken())
+    public Mono<ResponseEntity<LoginResponse>> loginOauth (
+            @RequestParam String id
+    ) {
+        LoginRequest request = LoginRequest.builder()
+                .id(id)
                 .build();
+
+        return userService.loginOauth(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @PostMapping("/idcheck")
-    public boolean idCheck(
+    public Mono<ResponseEntity<IdCheckResponse>> idCheck (
             @RequestBody IdCheckRequest request
-    ){
-        User user = userService.getUserByUserId(request.getId());
-        if(user == null) {
-            return true;
-        }
-        return false;
+    ) {
+        return userService.idCheck(request)
+                .map(response -> new ResponseEntity<>(null, HttpStatus.OK));
     }
 
     @PostMapping("/password")
-    public Password resetPassword(@RequestBody PasswordResetRequest request) {
-        Password newPassword = userService.resetPassword(request);
-
-        if(newPassword == null) {
-            return null;
-        }
-
-        return newPassword;
+    public Mono<ResponseEntity<PasswordResetResponse>> resetPassword (
+            @RequestBody PasswordResetRequest request
+    ) {
+        return userService.resetPassword(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @GetMapping("/token")
-    private AuthTokenResponse reissueToken (
+    private Mono<ResponseEntity<ReissueTokenResponse>> reissueToken (
             Authentication authentication
     ) {
-        if(authentication == null) {
-            return null;
-        }
-
-        UserDetail userDetail = (UserDetail) authentication.getDetails();
-        String id = userDetail.getUsername();
-        String refreshToken = authentication.getPrincipal().toString();
-
-        AuthToken token = userService.reissueAuthToken(id, refreshToken);
-
-        if(token == null) {
-            return null;
-        }
-
-        return AuthTokenResponse.builder()
-                .accessToken(token.getAccessToken().getAccessToken())
-                .refreshToken(token.getRefreshToken().getRefreshToken())
+        ReissueTokenRequest request = ReissueTokenRequest.builder()
+                .authentication(authentication)
                 .build();
+
+        return userService.reissueAuthToken(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @GetMapping
-    public UserInfoQueryResultResponse getUser(
+    public Mono<ResponseEntity<QueryUserInfoResponse>> getUserInfo (
             Authentication authentication
-    ){
-        if(authentication == null) {
-            return null;
-        }
-
-        UserDetail userDetail = (UserDetail) authentication.getDetails();
-        String userId = userDetail.getUsername();
-
-        User user = userService.getUserByUserId(userId);
-
-        if(user == null) {
-            return null;
-        }
-
-        return UserInfoQueryResultResponse.builder()
-                .id(user.getUserId())
-//                .password(user.getPassword())
-                .name(user.getUserName())
+    ) {
+        QueryUserInfoRequest request = QueryUserInfoRequest.builder()
+                .authentication(authentication)
                 .build();
+
+        return userService.getUserInfo(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @PostMapping("/register")
-    public UserInfo registerUser(
-            @RequestBody UserRegisterRequest request
+    public Mono<ResponseEntity<RegisterResponse>> registerUser(
+            @RequestBody RegisterRequest request
     ) {
-        User user = userService.createUser(request);
-
-        if(user == null) return null;
-        return UserInfo.builder()
-                .id(user.getUserId())
-                .name(user.getUserName())
-                .build();
+        return userService.createUser(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @PatchMapping
-    public UserInfo updateUser(
+    public Mono<ResponseEntity<UpdateResponse>> updateUser(
             Authentication authentication,
             @RequestBody PasswordUpdateRequest request
-    ){
-        if(authentication == null) {
-            return null;
-        }
+    ) {
+        request.setAuthentication(authentication);
 
-        UserDetail userDetail = (UserDetail) authentication.getDetails();
-        String userId = userDetail.getUsername();
-
-        User user = userService.updateUser(userId, request);
-
-        if(user == null) return null;
-        return UserInfo.builder()
-                .id(user.getUserId())
-                .name(user.getUserName())
-                .build();
+        return userService.updateUser(request)
+                .map(response -> new ResponseEntity<>(response, HttpStatus.OK));
     }
 
     @DeleteMapping
-    public String deleteUser(
+    public Mono<ResponseEntity<DeleteResponse>> deleteUser(
             Authentication authentication
-    ){
-        if(authentication == null) {
-            return null;
-        }
+    ) {
+        DeleteRequest request = DeleteRequest.builder()
+                .authentication(authentication)
+                .build();
 
-        UserDetail userDetail = (UserDetail) authentication.getDetails();
-        String userId = userDetail.getUsername();
-
-        boolean result = userService.deleteUser(userId);
-        if(result) return "탈퇴 성공";
-        return "탈퇴 실패";
+        return userService.deleteUser(request)
+                .map(response -> new ResponseEntity<>(null, HttpStatus.OK));
     }
 }
