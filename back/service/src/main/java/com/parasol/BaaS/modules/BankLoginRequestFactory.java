c1e4ef83af4eacefbf51235ebbdd6d910064e53a
김형준 <ninini6630@gmail.com>
2022-05-12 15:49:45 +0900

    Refactor : :bulb: [BE] BaaS ↔ Main 간 뱅킹 로그인 요청 캡슐화

--- back/service/src/main/java/com/parasol/BaaS/modules/BankLoginRequestFactory.java
+++ back/service/src/main/java/com/parasol/BaaS/modules/BankLoginRequestFactory.java
@@ -1,22 +1,15 @@
 package com.parasol.BaaS.modules;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.parasol.BaaS.api_request.BankLoginRequest;
-import com.parasol.BaaS.api_request.DepositRequest;
-import com.parasol.BaaS.api_response.BankLoginResultResponse;
-import com.parasol.BaaS.api_response.TransactionExecuteResultResponse;
+import com.parasol.BaaS.api_param.BankLoginParam;
+import com.parasol.BaaS.api_result.BankLoginResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 
 @Component
@@ -29,18 +22,16 @@ public class BankLoginRequestFactory {
     @Value("${sbj-api-server.login}")
     private String endPoint;
 
-    public BankLoginResultResponse create(BankLoginRequest request) {
+    public Mono<BankLoginResult> create(BankLoginParam request) {
         WebClient.UriSpec<WebClient.RequestBodySpec> uriSpec = fixedText.method(HttpMethod.POST);
         WebClient.RequestHeadersSpec<?> bodySpec = uriSpec.uri(uriBuilder -> uriBuilder
                         .path(endPoint)
                         .build()
-                ).contentType(MediaType.APPLICATION_JSON)
+                )
                 .body(BodyInserters
                         .fromValue(request)
                 );
 
-        Mono<BankLoginResultResponse> response = bodySpec.retrieve().bodyToMono(BankLoginResultResponse.class);
-
-        return response.block();
+        return bodySpec.retrieve().bodyToMono(BankLoginResult.class);
     }
 }
