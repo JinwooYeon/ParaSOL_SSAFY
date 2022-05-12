ce0a21a138bc474c7a54517d9246c3f2779cdd6b
조항준 <wngkdroqkf441@gmail.com>
2022-05-12 14:45:33 +0900

    Feat : ✨ [FE] 고객 문의 페이지 하이퍼링크 기능 추가

--- front/app/screens/MypageStack/Service.tsx
+++ front/app/screens/MypageStack/Service.tsx
@@ -1,17 +1,32 @@
 import BtnBox from "../../components/BtnBox";
 import {
-  ContentContainer,
   ContentFooterContainer,
   FooterContainer,
   HeaderText,
   LayoutContainer,
 } from "../styled";
+import { Linking, Text } from "react-native";
+import styled from "styled-components/native";
+
+const ContentContainer = styled.View`
+  flex: 1;
+  margin: auto;
+  justify-content: center;
+`;
 
 const Service = ({ navigation }: any) => (
   <LayoutContainer>
     <HeaderText>고객 문의</HeaderText>
     <ContentFooterContainer>
-      <ContentContainer></ContentContainer>
+      <ContentContainer>
+        <Text
+          onPress={() =>
+            Linking.openURL("https://lab.ssafy.com/s06-final/S06P31S101")
+          }
+        >
+          싸피 깃 바로가기
+        </Text>
+      </ContentContainer>
       <FooterContainer>
         <BtnBox color="white" text="뒤로" navigation={navigation} />
       </FooterContainer>
