bb87d83d54842a5f27457bc0fabe6633ee5b56e9
선민기 <suns1502@gmail.com>
2022-05-13 16:22:46 +0900

    Feat : ✨ [BE] 서버 분리 - 출금 Init

---
+++ back/main-withdraw/src/test/java/com/parasol/mainwithdraw/MainWithdrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasol.mainwithdraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainWithdrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
