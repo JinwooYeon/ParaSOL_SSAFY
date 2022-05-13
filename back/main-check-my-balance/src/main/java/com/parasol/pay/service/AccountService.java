facdc5eaf863f8c5fdbf3ed1a3ae706e2fec38bf
선민기 <suns1502@gmail.com>
2022-05-13 12:00:10 +0900

    Feat : ✨ [BE] 서버 분리 - 잔액조회 서비스

---
+++ back/main-check-my-balance/src/main/java/com/parasol/pay/service/AccountService.java
@@ -0,0 +1,56 @@
+package com.parasol.pay.service;
+
+import com.parasol.pay.api_request.AccountBalanceQueryParam;
+import com.parasol.pay.api_request.AccountBalanceQueryRequest;
+import com.parasol.pay.api_request.AccountOpenRequest;
+import com.parasol.pay.api_request.LoginParam;
+import com.parasol.pay.api_response.AccountBalanceQueryResultResponse;
+import com.parasol.pay.api_response.LoginResult;
+import com.parasol.pay.modules.OpenAccountRequestFactory;
+import com.parasol.pay.modules.QueryAccountBalanceRequestFactory;
+import com.parasol.pay.modules.UserLoginSocketRequestFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AccountService {
+    @Autowired
+    private OpenAccountRequestFactory openAccountRequestFactory;
+    @Autowired
+    private QueryAccountBalanceRequestFactory queryAccountBalanceRequestFactory;
+    @Autowired
+    private UserLoginSocketRequestFactory userLoginSocketRequestFactory;
+
+    public Mono<String> create(AccountOpenRequest request) {
+        return openAccountRequestFactory.createOpenAccountRequest(request);
+    }
+
+
+    public Mono<AccountBalanceQueryResultResponse> getBalance(AccountBalanceQueryRequest request) {
+        LoginParam loginParam = LoginParam.builder()
+                .id(request.getId())
+                .password(request.getPassword())
+                .build();
+
+        return userLoginSocketRequestFactory.userLoginRequest(loginParam)
+                .filter(LoginResult::getIsSuccess)
+                .flatMap(
+                        loginResult -> {
+                            Long cusNo = loginResult.getCusNo();
+
+                            AccountBalanceQueryParam param = AccountBalanceQueryParam.builder()
+                                    .cusNo(cusNo)
+                                    .accountNumber(request.getAccountNumber())
+                                    .build();
+
+                            return queryAccountBalanceRequestFactory.createQueryAccountBalanceRequest(param)
+                                    .map(queryResult ->
+                                            AccountBalanceQueryResultResponse.builder()
+                                                    .balance(queryResult.getBalance())
+                                                    .build()
+                                    );
+                        }
+                );
+    }
+}
