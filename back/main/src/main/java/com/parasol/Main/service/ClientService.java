27a146163c9b869ec75f5bb6b36da6cf88fe6214
김형준 <ninini6630@gmail.com>
2022-05-13 16:27:31 +0900

    Refactor : :bulb: [BE] 미구현 및 불필요한 API 삭제 (고객 정보 조회, 삭제 등)

--- back/main/src/main/java/com/parasol/Main/service/ClientService.java
+++ back/main/src/main/java/com/parasol/Main/service/ClientService.java
@@ -5,7 +5,6 @@ import com.parasol.Main.api_model.ClientInfo;
 import com.parasol.Main.api_request.ClientRegisterRequest;
 import com.parasol.Main.api_response.ClientCreateResponse;
 import com.parasol.Main.modules.CreateAddClientRequest;
-import com.parasol.Main.modules.FindClientRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -16,18 +15,8 @@ public class ClientService {
     @Autowired
     private CreateAddClientRequest addClientRequest;
 
-    @Autowired
-    private FindClientRequest findClientRequest;
-
     public Mono<ClientCreateResponse> create(ClientRegisterRequest request) {
         return addClientRequest.createAddClientRequest(request);
     }
-
-    public Mono<Client> findById(String id){
-        return findClientRequest.findClientRequest(id);
-    }
-
-    public ClientInfo findByResidentNumber(String residentNumber){
-        return null;
-    }
+    
 }
