1cdc80ecd35348cafb8050a57306bd26acafecb7
선민기 <suns1502@gmail.com>
2022-05-13 15:46:01 +0900

    Feat : ✨ [BE] 서버 분리 - 입금 테스트

---
+++ back/main-deposit/src/test/java/com/parasol/maindeposit/MainDepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasol.maindeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainDepositApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
