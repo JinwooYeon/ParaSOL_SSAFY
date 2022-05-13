8b4a5dec6048861ac3ef71fb8410e7ca8f888025
선민기 <suns1502@gmail.com>
2022-05-13 16:23:17 +0900

    Feat : ✨ [BE] 서버 분리 - 출금 API 모델 정의

---
+++ back/main-withdraw/src/main/java/com/parasol/mainwithdraw/api_response/TransactionExecutionResultResponse.java
@@ -0,0 +1,14 @@
+package com.parasol.mainwithdraw.api_response;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@ApiModel("TransactionExecutionResultResponse")
+public class TransactionExecutionResultResponse {
+    private boolean success = false;
+}
