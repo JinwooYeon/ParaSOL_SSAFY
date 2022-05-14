9de64c096106f2cf7ed0fd152ff87347205c696e
오나연 <cjcm1525@naver.com>
2022-05-14 21:54:46 +0900

    Feat : ✨ [BE] main 서버 분리 - 사용자 인증

---
+++ back/main_authentication/src/main/java/com/parasol/main_authentication/service/AccountAuthenticateService.java
@@ -0,0 +1,28 @@
+package com.parasol.main_authentication.service;
+
+import com.parasol.main_authentication.entity.ApiKey;
+import com.parasol.main_authentication.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountAuthenticateService extends AuthenticateService{
+    private static final String pathName = "/account";
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public boolean isValid(String ipAddr, String method, String endpoint, String clientId){
+        if(!super.isValid(ipAddr, method, endpoint, clientId))
+            return false;
+
+        ApiKey apiKey = new ApiKey();
+        apiKey.setClientId(clientId);
+
+        if(accountRepository.findByMethodAndClientIdAndPermitEndpoint(getMethodType(method), apiKey, getSubpath(pathName, endpoint)) == null)
+            return false;
+
+        return true;
+    }
+}
