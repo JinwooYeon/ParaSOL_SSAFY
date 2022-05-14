33d9a0f96ca44de17cacab4de32800636a3cce26
김형준 <ninini6630@gmail.com>
2022-05-14 11:16:39 +0900

    Style : 🎨 개행 스타일 조정

--- back/main/src/main/java/com/parasol/Main/controller/AuthController.java
+++ back/main/src/main/java/com/parasol/Main/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.parasol.Main.controller;
 
 import com.parasol.Main.api_request.RegisterRequest;
+import com.parasol.Main.api_response.RegisterResponse;
 import com.parasol.Main.service.ApiKeyAuthenticateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/auth")
@@ -12,7 +14,9 @@ public class AuthController {
     private ApiKeyAuthenticateService apiKeyAuthenticateService;
 
     @PostMapping("register")
-    public String register(@RequestBody(required = true) RegisterRequest registerRequest) {
+    public Mono<RegisterResponse> register(
+            @RequestBody(required = true) RegisterRequest registerRequest
+    ) {
         return apiKeyAuthenticateService.register(registerRequest);
     }
 }
