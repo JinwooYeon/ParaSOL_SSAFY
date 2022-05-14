809e0ae74f869b19ea8c2b53a597d5d538dda5aa
김형준 <ninini6630@gmail.com>
2022-05-14 20:43:27 +0900

    Refactor : :bulb: [BE] 메인서버 입금 관련 코어 접속 오류 처리 코드 추가

--- back/main-deposit/src/main/java/com/parasol/maindeposit/service/AccountService.java
+++ back/main-deposit/src/main/java/com/parasol/maindeposit/service/AccountService.java
@@ -6,7 +6,10 @@ import com.parasol.maindeposit.api_response.DepositResponse;
 import com.parasol.maindeposit.eenum.TransactionType;
 import com.parasol.maindeposit.modules.DepositRequestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -23,6 +26,14 @@ public class AccountService {
                 .build();
 
         return depositRequestFactory.run(param)
+                .doOnError( (throwable) -> {
+                    WebClientResponseException ex = (WebClientResponseException)throwable;
+
+                    if (ex.getStatusCode().is4xxClientError())
+                        throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+                    else if (ex.getStatusCode().is5xxServerError())
+                        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+                })
                 .map(queryResult ->
                         DepositResponse.builder()
                                 .isSuccess(queryResult.isSuccess())
