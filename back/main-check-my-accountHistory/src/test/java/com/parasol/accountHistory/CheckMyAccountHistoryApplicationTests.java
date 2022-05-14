6ba81736b2a5e7cc570e2969d0c5cb46cda1fd08
김나경 <skruddl50@gmail.com>
2022-05-14 11:36:18 +0900

    Feat : ✨ [BE] 서버 분리 - 거래내역조회

---
+++ back/main-check-my-accountHistory/src/test/java/com/parasol/accountHistory/CheckMyAccountHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parasol.accountHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckMyAccountHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
