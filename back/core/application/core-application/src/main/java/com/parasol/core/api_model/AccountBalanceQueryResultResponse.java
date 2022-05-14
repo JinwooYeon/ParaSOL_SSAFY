3f4d84f7760127641d851f463c9bd3d417bdd06b
김형준 <ninini6630@gmail.com>
2022-05-14 15:26:35 +0900

    Refactor : :bulb: [BE] 계정계 서버 잔액조회 반환 DTO Builder 패턴 도입

--- back/core/application/core-application/src/main/java/com/parasol/core/api_model/AccountBalanceQueryResultResponse.java
+++ back/core/application/core-application/src/main/java/com/parasol/core/api_model/AccountBalanceQueryResultResponse.java
@@ -2,12 +2,16 @@ package com.parasol.core.api_model;
 
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @ToString
+@SuperBuilder
+@NoArgsConstructor
 @ApiModel("AccountBalanceQueryResultResponse")
 public class AccountBalanceQueryResultResponse extends AccountBalance {
 }
