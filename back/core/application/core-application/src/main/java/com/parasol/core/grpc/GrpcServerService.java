9bde9f69341a7035a51535156dd795b918b1945f
김나경 <skruddl50@gmail.com>
2022-05-15 18:59:46 +0900

    Feat : ✨ [BE] Core 서버 입금 GrpcServerService 구현

--- back/core/application/core-application/src/main/java/com/parasol/core/grpc/GrpcServerService.java
+++ back/core/application/core-application/src/main/java/com/parasol/core/grpc/GrpcServerService.java
@@ -1,6 +1,7 @@
 package com.parasol.core.grpc;
 
 import com.parasol.core.api_model.*;
+import com.parasol.core.eenum.TransactionType;
 import com.parasol.core_interface.*;
 import com.parasol.core.service.AccountService;
 import com.parasol.core.service.BankUserService;
@@ -94,6 +95,33 @@ public class GrpcServerService extends CoreAPIGrpc.CoreAPIImplBase {
 
             AccountBalanceQueryGrpcResponse response = AccountBalanceQueryGrpcResponse.newBuilder()
                     .setDepAcBlc(queryResponse.getBalance().toString())
+                    .build();
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (ResponseStatusException ex) {
+            responseObserver.onError(ex);
+        }
+    }
+
+    @Override
+    public void deposit(DepositQueryGrpcRequest request, StreamObserver<DepositQueryGrpcResponse> responseObserver) {
+        try {
+            Long amount = Long.parseLong(request.getTrxAmt().trim());
+            String accountTo = request.getDepAcno().substring(0,3) + "-" + request.getDepAcno().substring(3,6) + "-" + request.getDepAcno().substring(6,12);
+            String nameFrom = request.getDpstPnNm();
+
+            DepositRequest queryRequest = DepositRequest.builder()
+                    .type(TransactionType.DEPOSIT)
+                    .accountTo(AccountInfo.builder().accountNumber(accountTo).build())
+                    .amount(amount)
+                    .nameOpponent(nameFrom)
+                    .build();
+
+            DepositResponse queryResponse = accountService.deposit(queryRequest);
+
+            DepositQueryGrpcResponse response = DepositQueryGrpcResponse.newBuilder()
+                    .setIsSuccess("1")
                     .build();
 
             responseObserver.onNext(response);
