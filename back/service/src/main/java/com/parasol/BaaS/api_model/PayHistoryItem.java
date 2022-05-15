211c59e463227197e15d2649c53bafeb7bc96449
오나연 <cjcm1525@naver.com>
2022-05-16 01:16:27 +0900

    Fix : :bug: [BE] price 타입 변환

--- back/service/src/main/java/com/parasol/BaaS/api_model/PayHistoryItem.java
+++ back/service/src/main/java/com/parasol/BaaS/api_model/PayHistoryItem.java
@@ -1,13 +1,20 @@
 package com.parasol.BaaS.api_model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@Builder
 public class PayHistoryItem {
     @NotNull
     private LocalDateTime id; // 날짜데이터
     @NotNull
     private String title; // 사용처
     @NotNull
-    private Long price;
+    private String price;  // 앞에 + -
 }
