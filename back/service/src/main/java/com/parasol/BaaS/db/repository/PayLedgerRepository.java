94214f62fddf931d576f732830e582d029ef2e2d
오나연 <cjcm1525@naver.com>
2022-05-15 19:31:57 +0900

    Feat : :sparkles: [BE] 아이디로 페이 정보 조회

--- back/service/src/main/java/com/parasol/BaaS/db/repository/PayLedgerRepository.java
+++ back/service/src/main/java/com/parasol/BaaS/db/repository/PayLedgerRepository.java
@@ -4,6 +4,9 @@ import com.parasol.BaaS.db.entity.PayHistory;
 import com.parasol.BaaS.db.entity.PayLedger;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PayLedgerRepository extends JpaRepository<PayLedger, Long> {
+    Optional<PayLedger> findByOwnerUserId(String id);
     
 }
