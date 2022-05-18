addde71a7ad9bf68512e0582ab80f3f79558b8db
김형준 <ninini6630@gmail.com>
2022-05-19 00:48:25 +0900

    Fix : :bug: [BE] BaaS 미사용 엔티티, 리포 삭제

--- back/service/src/main/java/com/parasol/BaaS/db/repository/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.parasol.BaaS.db.repository;
-
-import com.parasol.BaaS.db.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-}
--- back/service/src/main/java/com/parasol/BaaS/db/repository/UserAccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parasol.BaaS.db.repository;
-
-import com.parasol.BaaS.db.entity.UserAccount;
-import com.parasol.BaaS.db.entity.UserAccountId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, UserAccountId> {
-}
