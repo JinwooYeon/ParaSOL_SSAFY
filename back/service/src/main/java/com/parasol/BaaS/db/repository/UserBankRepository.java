735caea30e85e2ef868c4c6c87ba465a26dc0fb7
김형준 <ninini6630@gmail.com>
2022-05-20 02:35:48 +0900

    Chore : 📝 [BE] 불필요한 BaaS Entity 삭제

--- back/service/src/main/java/com/parasol/BaaS/db/repository/UserBankRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parasol.BaaS.db.repository;
-
-import com.parasol.BaaS.db.entity.UserBank;
-import com.parasol.BaaS.db.entity.UserBankId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserBankRepository extends JpaRepository<UserBank, UserBankId> {
-}
